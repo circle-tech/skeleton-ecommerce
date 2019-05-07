413cd91439ceff7b6c63bc25211d36059cdd33fc
azrihafiz <azri_hafiz@hotmail.com>
2019-05-07 21:41:46 +0800

    Added URL for registering account

--- app/src/main/java/com/circletech/skeletonecommerce/API.java
+++ app/src/main/java/com/circletech/skeletonecommerce/API.java
@@ -4,4 +4,5 @@ public class API {
     private static final String ROOT_URL = "https://testazrihafiz.000webhostapp.com/circletech/skeleton-ecommerce/API.php?apicall=";
 
     public static final String URL_LOGIN_ACCOUNT = ROOT_URL + "loginacc";
+    public static final String URL_REGISTER_ACCOUNT = ROOT_URL + "registeracc";
 }

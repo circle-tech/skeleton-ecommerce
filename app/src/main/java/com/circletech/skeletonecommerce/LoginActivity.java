2e91e910a29e1f82354685e61fbeed08e40ac54e
azrihafiz <azri_hafiz@hotmail.com>
2019-05-07 13:31:19 +0800

    Added Java file for activity_login.xml

---
+++ app/src/main/java/com/circletech/skeletonecommerce/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.circletech.skeletonecommerce;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}

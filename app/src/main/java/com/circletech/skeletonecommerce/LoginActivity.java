13d79aa251dc7f81009b0d700ae48ab5ea6aefa7
firdaus5679 <firdaus.zakaria5679@gmail.com>
2019-05-07 15:50:38 +0800

    working login page from login page to main page

--- app/src/main/java/com/circletech/skeletonecommerce/LoginActivity.java
+++ app/src/main/java/com/circletech/skeletonecommerce/LoginActivity.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.jar.Attributes;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText Name;
+    private EditText Password;
     Button btnLogin;
 
     @Override
@@ -15,15 +20,21 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+                validate(Name.getText().toString(), Password.getText().toString());
             }
         });
+    }
+    private void validate(String userName, String userPassword){
+        if((userName.equals("admin")) && (userPassword.equals("1234"))){
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }

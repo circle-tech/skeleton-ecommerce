b38d3940f5ddb00f636bfad7d51bc774b427e9f2
azrihafiz <azri_hafiz@hotmail.com>
2019-05-07 16:00:15 +0800

    Added files to linked the app with online database

---
+++ app/src/main/java/com/circletech/skeletonecommerce/API.java
@@ -0,0 +1,7 @@
+package com.circletech.skeletonecommerce;
+
+public class API {
+    private static final String ROOT_URL = "https://testazrihafiz.000webhostapp.com/circletech/skeleton-ecommerce/API.php?apicall=";
+
+    public static final String URL_LOGIN_ACCOUNT = ROOT_URL + "loginacc";
+}
---
+++ app/src/main/java/com/circletech/skeletonecommerce/UserAccount.java
@@ -0,0 +1,24 @@
+package com.circletech.skeletonecommerce;
+
+import java.io.Serializable;
+
+public class UserAccount implements Serializable {
+
+    public static String userName;
+    public static String email;
+
+    public UserAccount(String userName, String email) {
+        this.userName = userName;
+        this.email = email;
+    }
+
+    //Getters
+    public String getUserName() { return userName; }
+
+    public String getEmail() { return email; }
+
+    //Setters
+    public void setUserName(String name) { this.userName = name; }
+
+    public void setEmail(String email) { this.email = email; }
+}

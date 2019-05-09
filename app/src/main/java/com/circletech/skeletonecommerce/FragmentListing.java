dfe4ff201374d463086e868ee252b3d2a76326c0
azrihafiz <azri_hafiz@hotmail.com>
2019-05-09 20:43:40 +0800

    Updated FragmentListing.java for backend processing.

--- app/src/main/java/com/circletech/skeletonecommerce/FragmentListing.java
+++ app/src/main/java/com/circletech/skeletonecommerce/FragmentListing.java
@@ -1,23 +1,119 @@
 package com.circletech.skeletonecommerce;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class FragmentListing extends Fragment {
+
+    private static final int CODE_GET_REQUEST = 1024;
+    private static final int CODE_POST_REQUEST = 1025;
+
+    List<Product> productList;
+
+    RecyclerView recyclerView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_listing, container, false);
+        View view = inflater.inflate(R.layout.fragment_listing, container, false);
+        recyclerView = view.findViewById(R.id.fragment_listing_RecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        productList = new ArrayList<>();
+        getProducts();
+        return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle("Listing Items");
+    }
+
+    private class PerformNetworkRequest extends AsyncTask<Void, Void, String> {
+        String url;
+        HashMap<String, String> params;
+        int requestCode;
+
+        PerformNetworkRequest(String url, HashMap<String, String> params, int requestCode) {
+            this.url = url;
+            this.params = params;
+            this.requestCode = requestCode;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONObject object = new JSONObject(s);
+                if (!object.getBoolean("error")) {
+                    //JSONArray array = new JSONArray(s);
+                    JSONArray array = object.getJSONArray("products");
+
+                    for (int i = 0; i < array.length(); i++) {
+
+                        JSONObject product = array.getJSONObject(i);
+
+                        productList.add(new Product(
+                                product.getInt("productId"),
+                                product.getString("productName"),
+                                product.getString("productDescription"),
+                                product.getInt("productQuantity"),
+                                product.getDouble("productPrice"),
+                                product.getString("productImage")
+                        ));
+                    }
+
+                    ProductAdapter productAdapter = new ProductAdapter(getActivity(), productList);
+                    recyclerView.setAdapter(productAdapter);
+                }
+                else {
+                    Toast.makeText(getActivity(), object.getString("message"), Toast.LENGTH_SHORT).show();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            APIRequestHandler requestHandler = new APIRequestHandler();
+
+            if (requestCode == CODE_POST_REQUEST)
+                return requestHandler.sendPostRequest(url, params);
+
+            if (requestCode == CODE_GET_REQUEST)
+                return requestHandler.sendGetRequest(url);
+
+            return null;
+        }
+    }
+
+    public void getProducts() {
+        FragmentListing.PerformNetworkRequest request = new FragmentListing.PerformNetworkRequest(API.URL_GET_PRODUCSTS, null, CODE_GET_REQUEST);
+        request.execute();
     }
 }

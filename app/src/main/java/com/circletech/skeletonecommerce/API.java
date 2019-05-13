3ed1c5e784113b295bdd89b4d964ce462431c665
azrihafiz <azri_hafiz@hotmail.com>
2019-05-14 00:48:16 +0800

    Added API URL to get product info.

--- app/src/main/java/com/circletech/skeletonecommerce/API.java
+++ app/src/main/java/com/circletech/skeletonecommerce/API.java
@@ -7,4 +7,5 @@ public class API {
     public static final String URL_REGISTER_ACCOUNT = ROOT_URL + "registeracc";
     public static final String URL_GET_PRODUCTS = ROOT_URL + "getproducts";
     public static final String URL_ADD_PRODUCT = ROOT_URL + "addproduct";
+    public static final String URL_GET_PRODUCT_INFO = ROOT_URL + "getproductinfo";
 }
